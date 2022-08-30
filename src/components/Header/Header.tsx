f26e5e7a0d9feffd1292cfdf6ba40fbf93a4c16d
Zurab Okroshidze <zurab.okroshidze@quantori.com>
2022-08-30 23:53:53 +0400

    add Header Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
+// import LangButtonComponent from '../langButton/LangButtonComponent';
+// import { Link } from 'react-router-dom';
 
-function Header(props: any) {
+// CSS
+import './header.scss';
+
+// Images
+// import patternSrc from '../../assets/img/pattern.png';
+import logoSrc from 'assets/logo.svg';
+// import { Language } from '../../models/Language';
+// import { useTranslation } from 'react-i18next';
+import { useApi } from 'hooks';
+
+interface HeaderComponentsProps {
+	languageClicked?: () => void;
+	// languages:Language[]
+}
+
+const Header:React.FC<HeaderComponentsProps> = (props :any) => {
+
+	const { data: backgroundImage, isLoading } = useApi<any>('https://cms.meamacollect.ge/meama-collect/api/client/get-background-image');
+	// const {t} = useTranslation();
+
+	console.log(backgroundImage,isLoading);
+
+
 	return (
-		<header className="header">
+		<div className="header-component">
+			<div className="bg">
+				<span style={ { backgroundImage: 'url('+ backgroundImage + ')' } }></span>
+			</div>
 			<div className="container">
-				<div>
-					<div className="box"><div className="logo"><img src="" alt=""/></div></div>
-					<div className="box"><div className="lang-changer"></div></div>
+				<div className="row no-gutters">
+					<div className="col-4 flex items-start" style={ {zIndex:999} }>
+						<div className="logo">
+							<img src={ logoSrc } alt=""/>
+						</div>
+					</div>
+					<div className="col-4 flex items-start justify-end" style={ {zIndex:999} }>
+						<button>lang</button>
+						{ /*<LangButtonComponent languages={ languages } languageClicked={ languageClicked }/>*/ }
+					</div>
 				</div>
 			</div>
-		</header>
+		</div>
 	);
-}
+};
 
 export default Header;
